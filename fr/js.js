4d22bb2202391123ba91b112560da0854163a065
aleksandrajankovic <aleksandra.bgd.87@gmail.com>
2023-11-13 23:05:35 +0100

    Update js.js

--- fr/js.js
+++ fr/js.js
@@ -7,13 +7,15 @@ document.addEventListener("DOMContentLoaded", function () {
   // Funkcija za preusmjeravanje korisnika na stranicu odabranog jezika
   function redirectToLanguage(selectedLang) {
     // Dodajte više "else if" blokova za druge jezike i odgovarajuće URL-ove
+    let pathArray = document.URL.split("/");
+      let secondLevelLocation = pathArray[5];
     if (selectedLang === "fr") {
-      window.location.href = "./";
+      window.location.href = "./" + secondLevelLocation;
     } else if (selectedLang === "nl") {
-      window.location.href = "../";
+      window.location.href = "../" + secondLevelLocation;
     }
     else if (selectedLang === "en") {
-      window.location.href = "../en";
+      window.location.href = "../en" + secondLevelLocation;
     }
   }
 
